796331c8ef74e7f4896b59403cf7c4d88cb6c030
Marlene Guzman <marleneguzmanla@gmail.com>
2019-03-12 14:51:44 -0700

    refactoring removing names

--- app.js
+++ app.js
@@ -2,10 +2,7 @@ const form = document.getElementById("registrar");
 const input = form.querySelector("input");
 const ul = document.getElementById("invitedList");
 
-form.addEventListener("submit", e => {
-  e.preventDefault();
-  const text = input.value;
-  input.value = "";
+function createLI (text) {
   const li = document.createElement("li");
   li.textContent = text;
   const label = document.createElement("label");
@@ -14,11 +11,18 @@ form.addEventListener("submit", e => {
   checkbox.type = "checkbox";
   label.appendChild(checkbox);
   li.appendChild(label);
-
   //removing names
   const button = document.createElement("button");
   button.textContent = "remove";
   li.appendChild(button);
+  return li;
+}
+
+form.addEventListener("submit", e => {
+  e.preventDefault();
+  const text = input.value;
+  input.value = "";
+  const li = createLI(text);
   ul.appendChild(li);
 });
 
