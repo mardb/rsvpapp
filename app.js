c694ef9ed195f65aff2e537b88e98f91c51cd12d
Marlene Guzman <marleneguzmanla@gmail.com>
2019-03-12 15:40:06 -0700

    inplementing editing capability

--- app.js
+++ app.js
@@ -1,47 +1,52 @@
-const form = document.getElementById("registrar");
-const input = form.querySelector("input");
-const ul = document.getElementById("invitedList");
+const form = document.getElementById('registrar');
+const input = form.querySelector('input');
+const ul = document.getElementById('invitedList');
 
 function createLI(text) {
-  const li = document.createElement("li");
-  li.textContent = text;
-  const label = document.createElement("label");
-  label.textContent = "Confirmed";
-  const checkbox = document.createElement("input");
-  checkbox.type = "checkbox";
+  const li = document.createElement('li');
+  const span = document.createElement('span');
+  span.textContent = text;
+  li.appendChild(span);
+  const label = document.createElement('label');
+  label.textContent = 'Confirmed';
+  const checkbox = document.createElement('input');
+  checkbox.type = 'checkbox';
   label.appendChild(checkbox);
   li.appendChild(label);
 
   //edit names
-  const editButton = document.createElement("button");
-  editButton.textContent = "edit";
+  const editButton = document.createElement('button');
+  editButton.textContent = 'edit';
   li.appendChild(editButton);
 
   //removing names
-  const removeButton = document.createElement("button");
-  removeButton.textContent = "remove";
+  const removeButton = document.createElement('button');
+  removeButton.textContent = 'remove';
   li.appendChild(removeButton);
   return li;
+  //save
+  const saveButton = document.createElement('button');
+  saveButton.textContent = 'save'
 }
 
-form.addEventListener("submit", e => {
+form.addEventListener('submit', e => {
   e.preventDefault();
   const text = input.value;
-  input.value = "";
+  input.value = '';
   const li = createLI(text);
   ul.appendChild(li);
 });
 
 //checks checkbox status
-ul.addEventListener("change", e => {
+ul.addEventListener('change', e => {
   const checkbox = event.target;
   const checked = checkbox.checked;
   const listItem = checkbox.parentNode.parentNode;
 
   if (checked) {
-    listItem.className = "responded";
+    listItem.className = 'responded';
   } else {
-    listItem.className = "";
+    listItem.className = '';
   }
 });
 
@@ -53,7 +58,21 @@ ul.addEventListener('click', (e) => {
     if (button.textContent === 'remove') {
       ul.removeChild(li);
     } else if (button.textContent === 'edit') {
-      console.log("edit");
+      const span = li.firstElementChild;
+      const input = document.createElement('input');
+      input.type= 'text';
+      input.value = span.textContent;
+      li.insertBefore(input, span);
+      li.removeChild(span);
+      button.textContent = 'save'
+    }else if (button.textContent === 'save') {
+      const input = li.firstElementChild;
+      const span = document.createElement('span')
+      span.textContent = input.value;
+      li.insertBefore(span, input);
+      li.removeChild(input);
+      button.textContent = 'edit'
     }
+
   }
 });
